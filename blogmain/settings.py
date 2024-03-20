6c840791b550265254912a45028689a8e42cc234
kroshini1 <adarikirankumar9.com>
2024-03-20 21:36:45 +0530

    login and logout and register functionality

--- blogmain/settings.py
+++ blogmain/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
